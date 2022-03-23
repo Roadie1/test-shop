e2b0de4cd50cd5277267685cc8bf202524b80776
Roadie <roadie321@mail.ru>
2022-03-23 13:06:25 +0300

    fixed not routing

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './front.js';
 
-const appRouting = (
-    <BrowserRouter>
-        <Routes>
-            <Route exact path="/" component={App} />
-        </Routes>
-    </BrowserRouter>
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );
-
-ReactDOM.render(appRouting, document.getElementById('root'));
