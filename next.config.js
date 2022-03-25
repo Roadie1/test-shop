9c952bcdad3e82bcf7c6eaee87adaa1c9452d456
Roadie <roadie321@mail.ru>
2022-03-25 17:07:26 +0300

    added typescript

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     distDir: 'build',
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.(ts|tsx)?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+        })
+        return config;
+    }
 }
