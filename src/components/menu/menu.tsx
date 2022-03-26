e869a86bec738d0a05593dfdd4b79904325ab6b7
Roadie <roadie321@mail.ru>
2022-03-26 16:05:01 +0300

    wip

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import clsx from 'clsx';
 import { CatalogIcon, SearchIcon } from '../../../public/icons';
 
 import styles from './menu.module.scss';
@@ -9,15 +10,50 @@ const Menu: React.FC<{}> = ({ }) => {
         console.log("catalog");
     }
 
+    const login = () => {
+        console.log("login");
+    }
+
+    const register = () => {
+        console.log("register");
+    }
+
+    const renderUserSection = () => {
+        const user = { cart: [{ id: '1' }, { id: '2' }], id: '1239', name: "TestUser"};
+        if (!user) {
+            return (
+                <div className={styles.AuthorizationContainer}>
+                    <div className={clsx(styles.Login, styles.Button)} onClick={login}>Login</div>
+                    <div className={styles.Button} onClick={register}>Register</div>
+                </div>
+            )
+        }
+        return (
+            <div className={styles.UserInfo}>
+                <div className={styles.Cart}>
+                    Cart: {user.cart.length}
+                </div>
+                <div className={styles.UserName}>
+                    {user.name}
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className={styles.Container}>
             <div className={styles.Catalog} onClick={openCatalog}>
-                <CatalogIcon className={styles.Icon}/>
+                <CatalogIcon className={styles.Icon} />
                 Catalog
             </div>
             <div className={styles.Search}>
-                <input className={styles.Input}/>
-                <SearchIcon className={styles.Icon}/>
+                <input className={styles.Input} />
+                <div className={styles.SearchButton}>
+                    <SearchIcon className={styles.Icon} />
+                </div>
+            </div>
+            <div className={styles.User}>
+                {renderUserSection()}
             </div>
         </div>
     )
